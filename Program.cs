40f557040cb17ba3718455913581247ec8ae52b3
darlyn <zomberkawaiimusic@gmail.com>
2024-06-30 02:10:57 -0400

    esto es un sistema de pub/sub emisor receptor desarrollado en c#

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using StackExchange.Redis;
+using System;
+namespace realTimeData
+{
+    class Program
+        //publisher
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            
+
+            // Configuración de Redis
+            string redisHost = Environment.GetEnvironmentVariable("REDIS_HOST")!;
+            int redisPort = int.Parse(Environment.GetEnvironmentVariable("REDIS_PORT")!); 
+            string redisPassword = Environment.GetEnvironmentVariable("REDIS_PASSWORD")!;
+            string channel = "testChannel";
+
+            ConfigurationOptions options = new ConfigurationOptions
+            {
+                EndPoints = { $"{redisHost}:{redisPort}" },
+                Password = redisPassword,
+                AllowAdmin = true
+            };
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(options);
+            IDatabase db = redis.GetDatabase();
+            ISubscriber sub = redis.GetSubscriber();
+
+            while (true)
+            {
+                Console.Write("Introduce un mensaje para publicar: ");
+                string message = Console.ReadLine()!;
+                sub.Publish(channel, message);
+                Console.WriteLine($"Publicado: {message}\n");
+            }
+        }
+    }
+}
---
+++ susbcriber/Program.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+using System;
+namespace subscriber {
+    class Program {
+        static void Main(String[] args){
+            Console.WriteLine("Hello world");
+
+            // Configuración de Redis
+            string redisHost = Environment.GetEnvironmentVariable("REDIS_HOST")!;
+            int redisPort = int.Parse(Environment.GetEnvironmentVariable("REDIS_PORT")!);  // Cambia al puerto correspondiente
+            string redisPassword = Environment.GetEnvironmentVariable("REDIS_PASSWORD")!;
+            string channel = "testChannel";
+            DateTime fechaYhora = DateTime.Now;
+
+            ConfigurationOptions options = new ConfigurationOptions{
+                EndPoints = {$"{redisHost}:{redisPort}"},
+                Password = redisPassword,
+                AllowAdmin = true
+            };
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(options);
+            ISubscriber sub = redis.GetSubscriber();
+
+
+
+            sub.Subscribe(channel, (channel, message) => {
+                Console.WriteLine($"Recibido: {message} \n Recibido: {fechaYhora}");
+            });
+
+            Console.WriteLine("Esperando mensajes: ");
+            Console.ReadLine();
+        }
+    }
+}
